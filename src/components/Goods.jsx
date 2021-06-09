c33a7c6c63060d3b0e3a6b3fa483f23e7a34abdd
Sergey Mysov <s.mysov@list.ru>
2021-06-10 00:17:07 +0400

    feat: implemented buttons show-more and hide-content

--- src/components/Goods.jsx
+++ src/components/Goods.jsx
@@ -1,15 +1,37 @@
+import { useState } from 'react';
 import GoodsItem from './GoodsItem';
 
 const Goods = ({ goods = [] }) => {
+  const [limit, setLimit] = useState(6);
+  const slicedGoods = () => goods.slice(0, limit);
+  const showMore = () => {
+    if (limit <= goods.length) {
+      setLimit(limit + 6);
+    }
+  };
+
+  const hideContent = () => slicedGoods().slice(0, setLimit(6));
+
   return (
     <section className='catalog'>
       <div className='container'>
         <h1 className='title'>Catalog</h1>
-        <ul className='goods'>
-          {goods.map((good) => (
-            <GoodsItem key={good.id} {...good} />
-          ))}
-        </ul>
+        <div className='catalog__inner'>
+          <ul className='goods'>
+            {slicedGoods().map((good) => (
+              <GoodsItem key={good.id} {...good} />
+            ))}
+          </ul>
+          {slicedGoods().length < goods.length ? (
+            <button className='catalog__show-more' onClick={showMore}>
+              Show more
+            </button>
+          ) : (
+            <button className='catalog__hide' onClick={hideContent}>
+              Hide content
+            </button>
+          )}
+        </div>
       </div>
     </section>
   );
